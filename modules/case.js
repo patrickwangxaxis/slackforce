571d5bfbf63f27109374a67f8e9ef2448f2313dd
Patrick Wang <patrick.wang@xaxis.com>
2017-07-12 15:21:18 -0400

    /

--- modules/case.js
+++ modules/case.js
@@ -27,8 +27,8 @@ exports.execute = (req, res) => {
         .then(data => {
             let fields = [];
             fields.push({title: "Subject", value: subject, short:false});
-            fields.push({title: "Description CCase", value: description, short:false});
-            fields.push({title: "Open in Salesforce Test:", value: oauthObj.instance_url + "/" + data.id, short:false});
+            fields.push({title: "Description Case", value: description, short:false});
+            fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + data.id, short:false});
             let message = {
                 text: "A new case has been created:",
                 attachments: [
