21304157a97aee7a5e518acf75b6c8e9224f7881
Patrick Wang <patrick.wang@xaxis.com>
2017-09-18 11:02:15 -0400

    test 20170918 1102am

--- modules/case.js
+++ modules/case.js
@@ -26,7 +26,6 @@ exports.execute = (req, res) => {
         })
         .then(data => {
             let fields = [];
-            fields.push({title: "Subject", value: subject, short:false});
             fields.push({title: "Description Case", value: description, short:false});
             fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + data.id, short:false});
             let message = {
