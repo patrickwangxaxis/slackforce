7b539a0efa74ee242ec3aec35f8b5193f0f321ab
Patrick Wang <patrick.wang@xaxis.com>
2017-09-20 11:38:15 -0400

    restore

--- server.js
+++ server.js
@@ -9,14 +9,8 @@ let express = require('express'),
     _case = require('./modules/case'),
     whoami = require('./modules/whoami'),
     actions = require('./modules/actions'),
-	
-	 
-    request = require('request'),
- 
-
-	
     app = express();
- 
+
 
 app.enable('trust proxy');
 
@@ -34,79 +28,8 @@ app.post('/case', _case.execute);
 app.post('/whoami', whoami.execute);
 app.post('/login', auth.loginLink);
 app.post('/logout', auth.logout);
- 
 app.get('/login/:slackUserId', auth.oauthLogin);
 app.get('/oauthcallback', auth.oauthCallback);
-
-app.post('/slash-commands/send-me-buttons', urlencodedParser, (req, res) =>{
-    res.status(200).end() // best practice to respond with empty 200 status code
-    var reqBody = req.body
-    var responseURL = reqBody.response_url
-    if (reqBody.token != YOUR_APP_VERIFICATION_TOKEN){
-        res.status(403).end("Access forbidden")
-    }else{
-        var message = {
-            "text": "This is your first interactive message",
-            "attachments": [
-                {
-                    "text": "Building buttons is easy right?",
-                    "fallback": "Shame... buttons aren't supported in this land",
-                    "callback_id": "button_tutorial",
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "yes",
-                            "text": "yes",
-                            "type": "button",
-                            "value": "yes"
-                        },
-                        {
-                            "name": "no",
-                            "text": "no",
-                            "type": "button",
-                            "value": "no"
-                        },
-                        {
-                            "name": "maybe",
-                            "text": "maybe",
-                            "type": "button",
-                            "value": "maybe",
-                            "style": "danger"
-                        }
-                    ]
-                }
-            ]
-        }
-        sendMessageToSlackResponseURL(responseURL, message)
-    }
-})
-
-function sendMessageToSlackResponseURL(responseURL, JSONmessage){
-    var postOptions = {
-        uri: responseURL,
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        json: JSONmessage
-    }
-    request(postOptions, (error, response, body) => {
-        if (error){
-            // handle errors as you see fit
-        }
-    })
-}
-
-app.post('/slack/actions', urlencodedParser, (req, res) =>{
-    res.status(200).end() // best practice to respond with 200 status
-    var actionJSONPayload = JSON.parse(req.body.payload) // parse URL-encoded payload JSON string
-    var message = {
-        "text": actionJSONPayload.user.name+" clicked: "+actionJSONPayload.actions[0].name,
-        "replace_original": false
-    }
-    sendMessageToSlackResponseURL(actionJSONPayload.response_url, message)
-})
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
