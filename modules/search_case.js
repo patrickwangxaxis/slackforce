df07afe9c9cbf6ac81845ea41049355c9ab9ddd4
Patrick Wang <patrick.wang@xaxis.com>
2017-09-25 17:59:44 -0400

    c

--- modules/search_case.js
+++ modules/search_case.js
@@ -28,6 +28,19 @@ exports.execute = (req, res) => {
                     fields.push({title: "Subject", value: c.Subject, short:true});
                     fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + c.Id, short:false});
                     attachments.push({color: "#A094ED", fields: fields});
+					attachments.push("actions": [
+                 
+                {
+                    "name": "change owner",
+                    "text": "Claim To Be The Case Owner",
+                    "style": "danger",
+                    "type": "button",
+                    "value": slackUserId
+                    
+                }
+            ])
+					
+					
                 });
                 res.json({text: "Cases matching '" + req.body.text + "':", attachments: attachments});
             } else {
