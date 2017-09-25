b6dd7358926cf67c7efd64ddfdb3b75cde2ce96b
Patrick Wang <patrick.wang@xaxis.com>
2017-09-25 19:06:10 -0400

    c

--- modules/search_case.js
+++ modules/search_case.js
@@ -19,27 +19,48 @@ exports.execute = (req, res) => {
         .then(data => {
             let caseResults = JSON.parse(data).records;
             if (caseResults && caseResults.length>0) {
-                let attachments = [];
-                caseResults.forEach(function(c) {
+                //let attachments = [];
+                //caseResults.forEach(function(c) {
                     let fields = [];
 					console.log('---case id is ' + c.Id);
                     fields.push({title: "Case Number", value: c.CaseNumber, short:true});
                     fields.push({title: "Owner", value: c.Owner.Name, short:true});
                     fields.push({title: "Subject", value: c.Subject, short:true});
                     fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + c.Id, short:false});
+					let message = {
+                text: "A new case has been created:",
+                attachments: [
+                    {color: "#F2CF5B", fields: fields,
+					"attachment_type": "default",
+            
+			"actions": [
+                 
+                {
+                    "name": "change owner",
+                    "text": "Claim Case Owner",
+                    "style": "danger",
+                    "type": "button",
+                    "value": slackUserId,
+                    "confirm": {
+                        "title": "",
+                        "text": "Are you sure?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ] 
+					}
+                ]
+            };
+					 
 					
-					let actions = [];
-					actions.push({name:"change owner", type:"button", value:slackUserId});
-					
-					
-                    attachments.push({color: "#A094ED", fields: fields, "attachment_type": "default", "actions": actions});
+                    //attachments.push({color: "#A094ED", fields: fields});
 					 
 					
 					 
 					
-                });
-                //res.json({text: "Cases matching '" + req.body.text + "':", attachments: attachments});
-				console.log('res.json is '+res.json({text: "Cases matching '" + req.body.text + "':", attachments: attachments}));
+                //});
+                res.json({text: "Cases matching '" + req.body.text + "':", attachments: attachments});
 				
             } else {
                 res.send("No records");
