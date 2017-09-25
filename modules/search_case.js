894b6520cb6eca1553457412074888baf19de625
Patrick Wang <patrick.wang@xaxis.com>
2017-09-25 15:49:01 -0400

    c

---
+++ modules/search_case.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let auth = require("./slack-salesforce-auth"),
+    force = require("./force"),
+    SEARCH_CASE_TOKEN = process.env.SLACK_SEARCH_CASE_TOKEN;
+
+exports.execute = (req, res) => {
+
+    if (req.body.token != SEARCH_CASE_TOKEN) {
+        res.send("Invalid token");
+        return;
+    }
+
+    let slackUserId = req.body.user_id,
+        oauthObj = auth.getOAuthObject(slackUserId),
+        q = "SELECT Id, casenumber, Owner.name, subject FROM Case WHERE subject LIKE '%" + req.body.text + "%' LIMIT 20";
+
+    force.query(oauthObj, q)
+        .then(data => {
+            let caseResults = JSON.parse(data).records;
+            if (caseResults && caseResults.length>0) {
+                let attachments = [];
+                caseResults.forEach(function(c) {
+                    let fields = [];
+                    fields.push({title: "Case Number", value: c.casenumber, short:true});
+                    fields.push({title: "Owner Name", value: c.owner.name, short:true});
+                    fields.push({title: "Subject", value: c.subject, short:true});
+                    fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + c.Id, short:false});
+                    attachments.push({color: "#A094ED", fields: fields});
+                });
+                res.json({text: "Cases matching '" + req.body.text + "':", attachments: attachments});
+            } else {
+                res.send("No records");
+            }
+        })
+        .catch(error => {
+            if (error.code == 401) {
+                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
+            } else {
+                res.send("An error as occurred");
+            }
+        });
+};
