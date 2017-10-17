cd4f17620ce729ae5ea9341aa962862c503706ec
Patrick Wang <patrick.wang@xaxis.com>
2017-10-17 16:22:19 -0400

    c

--- modules/button_action.js
+++ modules/button_action.js
@@ -25,8 +25,8 @@ exports.execute = (req, res) => {
         subject = "test subject",
         description = "test description",
 		caseId = "500e000000AmhVU",
-		ownerId = "005i0000005PQjt"; // pw
-		//ownerId = "00531000006n0UJ"; //VS
+		//ownerId = "005i0000005PQjt"; // pw
+		ownerId = "00531000006n0UJ"; //VS
 		
     if (actionName == "case button")
 	{		
